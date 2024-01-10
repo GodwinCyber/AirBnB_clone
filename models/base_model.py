11c6181805864aafb5e01fa670fd495ca6606103
GodwinCyber <godwinchuks032@gmail.com>
2024-01-10 07:27:58 -0800

    updated basemodel due to model storage

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,13 @@ class BaseModel:
                     self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """update the update_at with current datetime"""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """
